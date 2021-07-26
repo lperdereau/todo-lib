8aa9bf866582813d0eb446679f66e2c49467492e
Louis PERDEREAU <leolouis.perdereau@gmail.com>
2021-07-26 11:00:09 +0200

    fix: Switch to references usage in function

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,20 @@ use todolist::TodoList;
 
 static TODOLIST_ARRAY: Lazy<Mutex<Vec<TodoList>>> = Lazy::new(|| Mutex::new(vec![]));
 
-pub fn save_todolist(todolist: self::todolist::TodoList) -> Vec<self::todolist::TodoList> {
-    TODOLIST_ARRAY.lock().unwrap().push(todolist);
+pub fn save_todolist(todolist: &self::todolist::TodoList) -> Vec<self::todolist::TodoList> {
+    TODOLIST_ARRAY.lock().unwrap().push(todolist.clone());
     return TODOLIST_ARRAY.lock().unwrap().to_vec();
 }
 
-pub fn delete_todolist(todolist: self::todolist::TodoList) -> Vec<self::todolist::TodoList> {
-    unimplemented!()
+pub fn delete_todolist(todolist: &self::todolist::TodoList) -> Vec<self::todolist::TodoList> {
+    let index: usize = TODOLIST_ARRAY
+        .lock()
+        .unwrap()
+        .iter()
+        .position(|x| x.id == todolist.id)
+        .unwrap();
+    TODOLIST_ARRAY.lock().unwrap().remove(index);
+    return TODOLIST_ARRAY.lock().unwrap().to_vec();
 }
 
 pub fn list_todolists() -> Vec<self::todolist::TodoList> {
@@ -59,16 +66,13 @@ pub mod todolist {
             }
         }
 
-        pub fn add_todo(&mut self, todo: Todo) -> &mut Self {
-            self.todos.push(todo);
+        pub fn add_todo(&mut self, todo: &Todo) -> &mut Self {
+            self.todos.push(todo.clone());
             return self;
         }
-        pub fn delete_todo(&mut self, todo: Todo) -> &mut Self {
-            let index: usize = self
-                .todos
-                .iter()
-                .position(|x| x.id == todo.id)
-                .unwrap();
+
+        pub fn delete_todo(&mut self, todo: &Todo) -> &mut Self {
+            let index: usize = self.todos.iter().position(|x| x.id == todo.id).unwrap();
             self.todos.remove(index);
             return self;
         }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
-use todolist::{save_todolist, list_todolists};
+use todolist::{delete_todolist, list_todolists, save_todolist};
 use todolist::todo::Todo;
 use todolist::todolist::TodoList;
 
 fn main() {
     let mut todolist = TodoList::new(String::from("TodoList1"));
     println!("{:?}", todolist);
-
+    
     let todo = Todo::new(String::from("Todo1"), false);
     println!("{:?}", todo);
 
-    todolist.add_todo(todo);
+    todolist.add_todo(&todo);
 
     println!("Todos: {:?}", todolist.todos);
-    
+
+    todolist.todos[0].done = true;
     println!("");
 
-    println!("TodosLists: {:?}", save_todolist(todolist));
+    println!("Todos: {:?}", todolist.todos);
+    println!("");
+
+    println!("TodosLists: {:?}", save_todolist(&todolist));
+
+    todolist.delete_todo(&todo);
+    
     println!("TodosLists: {:?}", list_todolists());
+    println!("TodosLists: {:?}", delete_todolist(&todolist));
+
+    let todo2 = Todo {
+        ..todo
+    };
+    println!("{:?}", ..todo2);
 }
