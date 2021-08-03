a3b3427fa3aa1327fa125b4817462dbbb4970551
Louis PERDEREAU <leolouis.perdereau@gmail.com>
2021-08-03 17:36:09 +0200

    feat: Add tests, coverage

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,14 @@
 use once_cell::sync::Lazy;
 use std::sync::Mutex;
 
-use self::todolist::TodoList;
-
 static TODOLIST_ARRAY: Lazy<Mutex<Vec<TodoList>>> = Lazy::new(|| Mutex::new(vec![]));
 
-pub fn save_todolist(todolist: &self::todolist::TodoList) -> Vec<self::todolist::TodoList> {
+pub fn save_todolist(todolist: &self::TodoList) -> Vec<self::TodoList> {
     TODOLIST_ARRAY.lock().unwrap().push(todolist.clone());
     return TODOLIST_ARRAY.lock().unwrap().to_vec();
 }
 
-pub fn delete_todolist(todolist: &self::todolist::TodoList) -> Vec<self::todolist::TodoList> {
+pub fn delete_todolist(todolist: &self::TodoList) -> Vec<self::TodoList> {
     let index: usize = TODOLIST_ARRAY
         .lock()
         .unwrap()
@@ -21,60 +19,52 @@ pub fn delete_todolist(todolist: &self::todolist::TodoList) -> Vec<self::todolis
     return TODOLIST_ARRAY.lock().unwrap().to_vec();
 }
 
-pub fn list_todolists() -> Vec<self::todolist::TodoList> {
+pub fn list_todolists() -> Vec<self::TodoList> {
     return TODOLIST_ARRAY.lock().unwrap().to_vec();
 }
 
-pub mod todo {
-    use uuid::Uuid;
+use uuid::Uuid;
 
-    #[derive(Debug, Clone)]
-    pub struct Todo {
-        pub id: Uuid,
-        pub comment: String,
-        pub done: bool,
-    }
+#[derive(Debug, Clone, PartialEq)]
+pub struct Todo {
+    pub id: Uuid,
+    pub comment: String,
+    pub done: bool,
+}
 
-    impl Todo {
-        pub fn new(comment: String, done: bool) -> Self {
-            Self {
-                id: Uuid::new_v4(),
-                comment,
-                done,
-            }
+impl Todo {
+    pub fn new(comment: String, done: bool) -> Self {
+        Self {
+            id: Uuid::new_v4(),
+            comment,
+            done,
         }
     }
 }
 
-pub mod todolist {
-    use super::todo::Todo;
-    use uuid::Uuid;
+#[derive(Debug, Clone, PartialEq)]
+pub struct TodoList {
+    pub id: Uuid,
+    pub title: String,
+    pub todos: Vec<Todo>,
+}
 
-    #[derive(Debug, Clone)]
-    pub struct TodoList {
-        pub id: Uuid,
-        pub title: String,
-        pub todos: Vec<Todo>,
+impl TodoList {
+    pub fn new(title: String) -> Self {
+        Self {
+            id: Uuid::new_v4(),
+            title,
+            todos: vec![],
+        }
     }
 
-    impl TodoList {
-        pub fn new(title: String) -> Self {
-            Self {
-                id: Uuid::new_v4(),
-                title,
-                todos: vec![],
-            }
-        }
+    pub fn add_todo(&mut self, todo: &Todo) -> &mut Self {
+        self.todos.push(todo.clone());
+        self
+    }
 
-        pub fn add_todo(&mut self, todo: &Todo) -> &mut Self {
-            self.todos.push(todo.clone());
-            self
-        }
-
-        pub fn delete_todo(&mut self, todo: &Todo) -> &mut Self {
-            let index: usize = self.todos.iter().position(|x| x.id == todo.id).unwrap();
-            self.todos.remove(index);
-            self
-        }
+    pub fn delete_todo(&mut self, todo: &Todo) -> Todo {
+        let index: usize = self.todos.iter().position(|x| x.id == todo.id).unwrap();
+        self.todos.remove(index)
     }
 }
